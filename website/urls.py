46e03bafdea4c218d8278503e536e6fe71cbfdf0
ClementSuave <clement.sauve24@gmail.com>
2025-04-13 14:06:42 +0200

    add CGU RGPD mentions legales

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("dix_km/", views.dix_km, name="dix_km"),
     path("trente_km/", views.trente_km, name="trente_km"),
     path("marche/", views.marche, name="marche"),
+    path("politiquedeconfidentialite/", views.RGPD, name="RGPD"),
+    path("conditionsgeneralesdutilisation/", views.CGU, name="CGU"),
+    path("mentionslegales/", views.mentions_legales, name="mentions_legales"),
 ]
--- website/views.py
+++ website/views.py
@@ -10,3 +10,9 @@ def trente_km(request):
 	return render(request, "website/trente_km.html")
 def marche(request):
 	return render(request, "website/marche.html")
+def RGPD(request):
+	return render(request, "website/politique_de_confidentialité.html")
+def CGU(request):
+	return render(request, "website/conditions_générales_utilisation.html")
+def mentions_legales(request):
+	return render(request, "website/mentions_légales.html")
