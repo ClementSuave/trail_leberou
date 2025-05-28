579181ad0ecefa2352be789deada22e9d35acb58
ClementSuave <clement.sauve24@gmail.com>
2025-05-28 11:32:23 +0200

    reglement

--- website/static/website/js/brasserie.js
+++ website/static/website/js/brasserie.js
@@ -6,21 +6,6 @@ $(function () {
 
 });
 
-function deferVideo() {
-
-  //defer html5 video loading
-  $("video source").each(function() {
-    var sourceFile = $(this).attr("data-src");
-    $(this).attr("src", sourceFile);
-    var video = this.parentElement;
-    video.load();
-    // uncomment if video is not autoplay
-    //video.play();
-  });
-
-}
-window.onload = deferVideo;
-
 $(document).ready(function() {
 
   $('[data-toggle="tooltip"]').tooltip();
