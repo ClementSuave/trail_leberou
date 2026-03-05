14ec9eccb23a9deeefdf467a9dc360f4582bad9a
clement <clement.sauve24@gmail.com>
2026-03-05 11:50:35 +0100

    send email for benevoles

--- website/signals.py
+++ website/signals.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
 from .models import Benevole
 
 @receiver(post_save, sender=Benevole)
